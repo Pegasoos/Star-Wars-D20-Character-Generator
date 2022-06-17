147284b0083b5a6838e1fe6ec12b3837b89815c1
Pegasoos <circumambulation24@yahoo.com>
2022-06-17 19:37:56 -0400

    User Sequelize Model

---
+++ models/User.js
@@ -0,0 +1,54 @@
+const { Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+
+class User extends Model{
+    checkPassword(loginPw){
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
+
+User.init(
+    {
+        id:{
+            type:DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement:true
+        },
+        username:{
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password:{
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate:{
+                len:[6]
+            }
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    },
+    {
+        hooks:{
+            beforeCreate: async (newUserData) => {
+                try {
+                    newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                }
+                catch (err) {
+                    console.log(err)
+                }
+                return newUserData;
+            }
+        },
+        sequelize,
+        timestamps:false,
+        freezeTableName:true,
+        underscored:true,
+        modelName:"user",
+    }
+);
+module.exports = User;
