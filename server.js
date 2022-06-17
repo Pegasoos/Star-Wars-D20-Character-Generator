58c878a91534b13e8504d3b85689b2f64b2f4ebb
Pegasoos <circumambulation24@yahoo.com>
2022-06-17 19:18:41 -0400

    Sequelize Connection Added in sever/connect file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const sequelize = require('./config/connection')
 
 const dotenv = require('dotenv').config();
 
@@ -10,6 +11,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(PORT, () => {
-    console.log(`Serving at http://localhost:${PORT}`)
-});
+sequelize.sync({force: false}).then(() => {
+    app.listen(PORT, () => {
+        console.log(`Serving at http://localhost:${PORT}`);
+    });
+})
