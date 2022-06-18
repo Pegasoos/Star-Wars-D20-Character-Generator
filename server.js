927ebd93888c082d794eb7d4ad580ce82e526532
Pegasoos <circumambulation24@yahoo.com>
2022-06-17 20:56:59 -0400

    Added index for api/homepage routes

--- server.js
+++ server.js
@@ -7,10 +7,12 @@ const dotenv = require('dotenv').config();
 
 const PORT = process.env.PORT || 7777;
 
+const routes = require('./controllers')
+
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(routes);
 sequelize.sync({force: false}).then(() => {
     app.listen(PORT, () => {
         console.log(`Serving at http://localhost:${PORT}`);
