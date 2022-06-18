90d33678005e9bf6387b1302afe4a1c7f6d70100
Pegasoos <circumambulation24@yahoo.com>
2022-06-17 21:42:18 -0400

    Login route added to user routes

--- controllers/apiRoutes/userRoutes.js
+++ controllers/apiRoutes/userRoutes.js
@@ -15,5 +15,28 @@ router.post('/', async (req, res) => {
         res.status(400).json(err);
     }
 });
-
+router.post('/login', async (req, res) =>{
+    try{
+        const userData = await User.findOne({ where: {username:req.body.username} });
+        console.log(userData)
+        if(!userData){
+            res.status(400).json({message:"Incorrect Username or Password!"});
+            return
+        }
+        const validPassword = await userData.checkPassword(req.body.password);
+        if(!validPassword){
+            res.status(400).json({message:"Incorrect Username or Password!"});
+            return
+        }
+        req.session.save(() =>{
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+    
+            res.json({message:'You are now logged in!'});
+        })
+    }
+    catch (err){
+    res.status(400).json(err)
+    };
+    });
 module.exports = router;
