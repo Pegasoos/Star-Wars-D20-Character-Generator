c414d7c66bc94936dc9784b34818aa140ea55474
Pegasoos <circumambulation24@yahoo.com>
2022-06-19 13:27:26 -0400

    Increased password len in schema, user post tested

--- controllers/apiRoutes/index.js
+++ controllers/apiRoutes/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const characterRoutes = require('./characterRoutes');
 
-router.use('users', userRoutes);
-router.use('characters', characterRoutes);
+router.use('/users', userRoutes);
+router.use('/characters', characterRoutes);
 
 module.exports = router;
